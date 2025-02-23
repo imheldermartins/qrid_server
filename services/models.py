72e8b2469e996ea345925cc2d086970a49ae19e2
imheldermartins <helder.santos0209@gmail.com>
2025-02-23 17:42:02 -0300

    Remove unecessary pycache from app_modules

--- services/models.py
+++ services/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 
 class Service(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
@@ -9,4 +7,4 @@ class Service(models.Model):
     price = models.FloatField(default=0, null=False)
     rated = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False, blank=False)
+    updated_at = models.DateTimeField(auto_now=True)
